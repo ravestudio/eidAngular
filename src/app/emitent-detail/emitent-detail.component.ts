b862bed847a154c093215dbc695c7d3b80695605
ravestudio <ravestudio@mail.ru>
2018-08-15 09:44:07 +0100

    add canvasjs

--- src/app/emitent-detail/emitent-detail.component.ts
+++ src/app/emitent-detail/emitent-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import * as CanvasJS from '../../assets/canvasjs.min';
 
 import { Emitent }         from '../emitent';
 import { EmitentService }  from '../emitent.service';
@@ -20,6 +21,31 @@ export class EmitentDetailComponent implements OnInit {
 
   ngOnInit() {
     this.getEmitent();
+
+    let chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        color: "#014D65",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+      
+    chart.render();
   }
 
   getEmitent(): void {
